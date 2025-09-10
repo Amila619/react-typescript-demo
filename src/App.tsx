58ae7acd1b23f4230fdc234f698b16a21d1633af
Amila619 <amilasandeepa349@gmail.com>
2025-09-10 21:35:30 +0530

    - learned about styleProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 // import Heading from "./components/Heading";
 // import Button from "./components/Button";
-import Input from "./components/Input";
+// import Input from "./components/Input";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
       {/* <Heading>Hello</Heading> */}
       {/* <Button handleClick={(event) => console.log("Button Clicked", event)}/> */}
-      <Input value="" handleChange={(event => console.log(event))} />
+      {/* <Input value="" handleChange={(event => console.log(event))} /> */}
+      <Container
+        styles={{
+          width: "100px",
+          height: "100px",
+          borderRadius: "50%",
+          backgroundColor: "#007bff",
+        }}
+      />
     </>
   );
 }
